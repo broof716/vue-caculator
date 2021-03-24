60dad602aad9bbd256590c467b530ee731e4324c
Bryan Roof <broof716@gmail.com>
2021-03-23 19:38:46 -0700

    adding sign, percent, and append methods to calculator

--- src/components/Caculator.vue
+++ src/components/Caculator.vue
@@ -1,37 +1,59 @@
 <template>
   <div class="caculator">
-    <div class="display">.0032040234</div>
-    <div class="btn">C</div>
-    <div class="btn">+/-</div>
-    <div class="btn">%</div>
-    <div class="btn">÷</div>
-    <div class="btn">7</div>
-    <div class="btn">8</div>
-    <div class="btn">9</div>
-    <div class="btn">x</div>
-    <div class="btn">4</div>
-    <div class="btn">5</div>
-    <div class="btn">6</div>
-    <div class="btn">-</div>
-    <div class="btn">1</div>
-    <div class="btn">2</div>
-    <div class="btn">3</div>
-    <div class="btn">+</div>
-    <div class=" btn zero">0</div>
+    <div class="display">{{current || '0'}}</div>
+    <div @click="clear" class="btn">C</div>
+    <div @click="sign" class="btn">+/-</div>
+    <div @click="percent" class="btn">%</div>
+    <div class="btn operator">÷</div>
+    <div @click="append('7')" class="btn">7</div>
+    <div @click="append('8')" class="btn">8</div>
+    <div @click="append('9')" class="btn">9</div>
+    <div class="btn operator">x</div>
+    <div @click="append('4')" class="btn">4</div>
+    <div @click="append('5')" class="btn">5</div>
+    <div @click="append('6')" class="btn">6</div>
+    <div class="btn operator">-</div>
+    <div @click="append('1')" class="btn">1</div>
+    <div @click="append('2')" class="btn">2</div>
+    <div @click="append('3')" class="btn">3</div>
+    <div class="btn operator">+</div>
+    <div @click="append('0')" class=" btn zero">0</div>
     <div class="btn">.</div>
-    <div class="btn">=</div>
+    <div class="btn operator">=</div>
     
   </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      current: '',
+    }
+  },
+  methods: {
+    clear() {
+      this.current = '';
+    },
+    sign() {
+      this.current = this.current.charAt(0) === '-' ?
+        this.current.slice(1) : `-${this.current}`; 
+    },
+    percent () {
+      this.current = `${parseFloat(this.current) / 100}`
+    },
+    append (number) {
+      this.current = `${this.current}${number}`;
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .caculator {
+  width: 400px;
+  margin: 0 auto;
   font-size: 40px;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
@@ -40,7 +62,8 @@ export default {
 
 .display {
   grid-column: 1 / 5;
-  background-color: red;
+  background-color: #333;
+  color: white;
 }
 
 .zero {
@@ -48,7 +71,12 @@ export default {
 }
 
 .btn {
-  background-color: #eee;
-  border: 1px solid #333;
+  background-color: #f2f2f2;
+  border: 1px solid #999;
+}
+
+.operator {
+  background-color: orange;
+  color: white;
 }
 </style>
